b668eeec396ce58ff627daac4927dbfdbeeb7ddd
Meena <meenakshi6296@gmail.com>
2022-11-27 20:16:29 -0500

    Added Inheritance program

---
+++ Inheritance Example.py
@@ -0,0 +1,34 @@
+class CarModels:
+
+    def models(self):
+
+        print("Model1")
+        print("Model2")
+        print("Model3")
+class Variants:
+      def carvariants(self):
+          print("BV Variant")
+          print("Mid Variant")
+          print("Top Variant")
+
+class Commonfeatures:
+    def Tyres(self):
+        print("MRF Tyres")
+    def Engine(self):
+        print("Internal combution engine")
+    def Seater(self):
+        print("5 Seater")
+    def Gearsys(self):
+        print("Manual Gear System")
+    def ACvents(self):
+        print("Front and rear Ac vent")
+CM= CarModels()
+CM.models()
+Var=Variants()
+Var.carvariants()
+CF=Commonfeatures()
+CF.Tyres()
+CF.Engine()
+CF.Seater()
+CF.Gearsys()
+CF.ACvents()
